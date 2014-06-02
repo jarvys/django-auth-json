7a1de8a2f90739ece80c34142b36a8268b0ab3f1
Jarvys <yuhan534@126.com>
2014-06-02 17:20:47 +0800

    polish

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
 	name='django-auth-json',
-	version='0.0.2',
+	version='0.0.3',
 	description='replace the login url parameter of django auth decorator with json',
 	author='yangchen',
 	author_email='yuhan534@126.com',
 	url='https://github.com/jarvys/django-auth-json',
-	install_requires=['django_render_json'],
+	install_requires=['django-render-json'],
 	py_modules=['django_auth_json'],
 	scripts=[],
 	keywords=['django', 'auth', 'decorators', 'json']
